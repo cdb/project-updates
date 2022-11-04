2f932946310c82bc9d73d6e9496e2a0d39bbc3d2
Cameron Booth <cdb@github.com>
2022-11-04 15:01:25 -0700

    Slightly working version, no slack yet

--- src/index.js
+++ src/index.js
@@ -1,15 +1,112 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import core from "@actions/core";
+import github from "@actions/github";
+import GitHubProject from "github-project";
+
+const octokit = github.getOctokit(core.getInput("token"));
+const organization = core.getInput("organization");
+const projectNumber = parseInt(core.getInput("project_number"), 10);
+const storageRepo = core.getInput("storage_repo");
+const storagePath = core.getInput("storage_path");
+const committerName = core.getInput("committer_name");
+const committerEmail = core.getInput("committer_email");
+
+async function getOldItems() {
+  let items = {};
+  let sha = undefined;
+  try {
+    let contents = await octokit.rest.repos.getContent({
+      owner: organization,
+      repo: storageRepo,
+      path: storagePath,
+    });
+
+    items = JSON.parse(atob(contents.data.content));
+    sha = contents.data.sha;
+  } catch (err) {
+    console.error(err);
+  }
+  return { items, sha };
+}
+
+async function getNewItems() {
+  const project = new GitHubProject({
+    owner: organization,
+    number: projectNumber,
+    octokit: octokit,
+    fields: {
+      status: "status",
+    },
+  });
+
+  const items = await project.items.list();
+  let data = {};
+  for (const item of items) {
+    data[item.content.url] = {
+      title: item.fields.title,
+      status: item.fields.status,
+      labels: item.content.labels,
+      url: item.content.url,
+      closed: item.content.closed,
+      closedAt: item.content.closedAt,
+      merged: item.content.merged,
+      assignees: item.content.assignees,
+    };
+  }
+  return data;
+}
+
+async function saveItems(items, sha) {
+  try {
+    await octokit.rest.repos.createOrUpdateFileContents({
+      owner: organization,
+      repo: storageRepo,
+      path: storagePath,
+      message: "update",
+      content: Buffer.from(JSON.stringify(items, null, 2)).toString("base64"),
+      sha,
+      committer: {
+        name: committerName,
+        email: committerEmail,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function outputDiff(prev, next) {
+  let added = [];
+  let removed = [];
+  let changed = [];
+  for (const id in prev) {
+    if (!(id in next)) {
+      removed.push(prev[id]);
+    } else if (JSON.stringify(prev[id]) !== JSON.stringify(next[id])) {
+      changed.push(next[id]);
+    }
+  }
+  for (const id in next) {
+    if (!(id in prev)) {
+      added.push(next[id]);
+    }
+  }
+  console.log("added", added);
+  console.log("removed", removed);
+  console.log("changed", changed);
+
+  return { added, removed, changed };
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  let { items: oldItems, sha } = await getOldItems();
+  console.log("oldItems", oldItems);
+  console.log("sha", sha);
+
+  let newItems = await getNewItems();
+  console.log("newItems:", newItems);
+
+  await saveItems(newItems, sha);
+  await outputDiff(oldItems, newItems);
 } catch (error) {
   core.setFailed(error.message);
 }
