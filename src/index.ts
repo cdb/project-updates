30fb9cec8ab953fa090604c23f9eedf1bf3b7504
Cameron Booth <cdb@github.com>
2022-11-05 14:53:26 -0700

    Bit more type safety

--- src/index.ts
+++ src/index.ts
@@ -83,8 +83,21 @@ async function saveItems(items, sha) {
   }
 }
 
+interface Change {
+  title: string,
+  url: string,
+  previous_title?: string,
+  status?: { prev: string, next: string },
+  labels_added?: string[],
+  labels_removed?: string[],
+  assignees_added?: string[],
+  assignees_removed?: string[],
+  closed?: { prev: boolean, next: boolean },
+  merged?: { prev: boolean, next: boolean },
+}
+
 function buildChanges(prev, next) {
-  let changes: any = { title: next.title, url: next.url };
+  let changes: Change = { title: next.title, url: next.url };
   if (prev.title !== next.title) {
     changes.previous_title = prev.title;
   }
@@ -93,22 +106,22 @@ function buildChanges(prev, next) {
     changes.status = { prev: prev.status, next: next.status };
   }
 
-  let added_labels = difference(next.labels, prev.labels);
+  let added_labels: string[] = difference(next.labels, prev.labels);
   if (added_labels.length > 0) {
     changes.labels_added = added_labels;
   }
 
-  let remove_labels = difference(prev.labels, next.labels);
-  if (remove_labels.length > 0) {
-    changes.labels_removed = remove_labels;
+  let removed_labels: string[] = difference(prev.labels, next.labels);
+  if (removed_labels.length > 0) {
+    changes.labels_removed = removed_labels;
   }
 
-  let added_assignees = difference(next.assignees, prev.assignees);
+  let added_assignees: string[] = difference(next.assignees, prev.assignees);
   if (added_assignees.length > 0) {
     changes.assignees_added = added_assignees;
   }
 
-  let removed_assignees = difference(prev.assignees, next.assignees);
+  let removed_assignees: string[] = difference(prev.assignees, next.assignees);
   if (removed_assignees.length > 0) {
     changes.assignees_removed = removed_assignees;
   }
