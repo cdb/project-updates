62c6761e99f8dd74e0c31cf42c61a7591e893a6e
Cameron Booth <cdb@github.com>
2022-11-05 16:10:50 -0700

    Rename to .ts

--- src/helpers.js
+++
@@ -1,5 +0,0 @@
-import { debug } from '@actions/core';
-
-export function debug(name, obj) {
-  debug(`${name}: ${JSON.stringify(obj)}`);
-}
---
+++ src/helpers.ts
@@ -0,0 +1,5 @@
+import * as core from '@actions/core';
+
+export function debug(name, obj) {
+  core.debug(`${name}: ${JSON.stringify(obj)}`);
+}
