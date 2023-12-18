c0b634c938845541349efca365072cc7a2960ea3
Cameron Booth <cdb@github.com>
2023-12-18 12:34:57 -0800

    Try to fix fetch failures

--- src/api.ts
+++ src/api.ts
@@ -2,9 +2,11 @@ import GitHubProject from 'github-project';
 import { Octokit } from '@octokit/rest';
 import * as core from '@actions/core';
 import { debug } from './helpers';
+import fetch from "node-fetch";
 
 const storageOctokit = new Octokit({
-  auth: core.getInput('storage_token')
+  auth: core.getInput('storage_token'),
+  fetch: fetch,
 });
 
 const projectOrganization = core.getInput('project_organization');
